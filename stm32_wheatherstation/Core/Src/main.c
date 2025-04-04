a8fd994a05784a09ecb94c4c85ecb3105f5a2e3a
SSorawit <sowarit007@gmail.com>
2025-04-04 14:46:09 +0700

    WIP

---
+++ stm32_wheatherstation/Core/Src/main.c
@@ -0,0 +1,485 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define RX_BUF_SIZE 128
+
+uint8_t rxByte;
+uint8_t rx_data;                        // byte ที่รับจาก UART
+uint8_t rx_buffer_rain[RX_BUF_SIZE];        // เก็บข้อความเต็มชุด
+uint8_t rx_index = 0;
+uint8_t rx_ready = 0;
+uint8_t rx_buffer[16];
+uint8_t modbus_frame[8];
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart1;
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+void Build_Modbus_Request(uint8_t slave_addr, uint16_t start_reg, uint16_t quantity, uint8_t *frame_out);
+void RS485_Transmit(uint8_t *data, uint16_t size);
+void TempHumi();
+void Lihgt();
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_IT(&huart2, &rx_data, 1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  if (rx_ready)
+	  {
+	      rx_ready = 0;
+	      printf("recive : %s\n", rx_buffer_rain);
+	      HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
+	      float accumulation = 0.0f;
+	      float eventAcc = 0.0f;
+	      float iph = 0.0f;
+
+	      char *accStr = strstr((char*)rx_buffer_rain, "Acc");
+	      char *eventStr = strstr((char*)rx_buffer_rain, "EventAcc");
+	      char *rintStr = strstr((char*)rx_buffer_rain, "RInt");
+
+	      if (accStr && sscanf(accStr, "Acc %f", &accumulation) != 1) accumulation = 0.0f;
+	      if (eventStr && sscanf(eventStr, "EventAcc %f", &eventAcc) != 1) eventAcc = 0.0f;
+	      if (rintStr && sscanf(rintStr, "RInt %f", &iph) != 1) iph = 0.0f;
+
+	      float accumulationMM = accumulation * 25.4f;
+	      float eventAccMM = eventAcc * 25.4f;
+	      float iphMM = iph * 25.4f;
+
+	      printf("accumulationMM: %.2f mm\n", accumulationMM);
+	      printf("eventAccMM: %.2f mm\n", eventAccMM);
+	      printf("iphMM: %.2f mm/hr\n", iphMM);
+	      printf("--------------------------------------\n");
+	  }
+	  TempHumi();
+	  Lihgt();
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK3|RCC_CLOCKTYPE_HCLK
+                              |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
+                              |RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK3Divider = RCC_SYSCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_RS485Ex_Init(&huart1, UART_DE_POLARITY_HIGH, 0, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 9600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5|GPIO_PIN_10, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : PB5 PB10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA9 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+int _write(int file, char *ptr, int len) {
+    for (int i = 0; i < len; i++) {
+        ITM_SendChar((*ptr++));
+    }
+    return len;
+}
+
+uint16_t Modbus_CRC16(uint8_t *buf, uint8_t len) {
+    uint16_t crc = 0xFFFF;
+
+    for (uint8_t pos = 0; pos < len; pos++) {
+        crc ^= (uint16_t)buf[pos];
+        for (int i = 0; i < 8; i++) {
+            if (crc & 0x0001) {
+                crc >>= 1;
+                crc ^= 0xA001;
+            } else {
+                crc >>= 1;
+            }
+        }
+    }
+
+    return crc;
+}
+
+void Build_Modbus_Request(uint8_t slave_addr, uint16_t start_reg, uint16_t quantity, uint8_t *frame_out) {
+    frame_out[0] = slave_addr;                // AA
+    frame_out[1] = 0x03;                      // Function Code
+
+    frame_out[2] = (start_reg >> 8) & 0xFF;   // RRRR (high byte)
+    frame_out[3] = start_reg & 0xFF;          // RRRR (low byte)
+
+    frame_out[4] = (quantity >> 8) & 0xFF;    // NNNN (high byte)
+    frame_out[5] = quantity & 0xFF;           // NNNN (low byte)
+
+    uint16_t crc = Modbus_CRC16(frame_out, 6);  // คำนวณ CRC จาก 6 byte แรก
+
+    frame_out[6] = crc & 0xFF;               // CRC Low byte
+    frame_out[7] = (crc >> 8) & 0xFF;        // CRC High byte
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART2)
+    {
+        if (rx_index < RX_BUF_SIZE - 1)
+        {
+        	rx_buffer_rain[rx_index++] = rx_data;
+
+            if (rx_data == '\n')  // จบข้อความ
+            {
+            	rx_buffer_rain[rx_index] = '\0'; // ปิดท้าย string
+                rx_ready = 1;
+                rx_index = 0;
+            }
+        }
+        else
+        {
+            rx_index = 0; // ถ้าเกินขนาด buffer
+        }
+
+        HAL_UART_Receive_IT(&huart2, &rx_data, 1); // รอ byte ถัดไป
+    }
+}
+
+void Decode_TempHumi(uint8_t *rx_buf) {
+    int16_t temp_raw = (rx_buf[3] << 8) | rx_buf[4];
+    uint16_t humi_raw = (rx_buf[5] << 8) | rx_buf[6];
+    int16_t dew_raw = (rx_buf[7] << 8) | rx_buf[8];
+    uint16_t press_raw = (rx_buf[9] << 8) | rx_buf[10];
+
+    float temp_c = temp_raw / 100.0f;  // °C
+    float humidity = humi_raw / 100.0f;  // %
+    float dewpoint_c = dew_raw / 100.0f;  // °C
+    float pressure_hpa = press_raw / 10.0f;  // hPa
+
+    printf("Temperature: %.2f C\r\n", temp_c);
+    printf("Humidity: %.2f %%\r\n", humidity);
+    printf("Dew Point: %.2f C\r\n", dewpoint_c);
+    printf("Pressure: %.1f hPa\r\n", pressure_hpa);
+}
+
+void Decode_Light(uint8_t *rx_buf) {
+    // อ่านค่า High และ Low 16 bits ของ Illuminance
+    uint16_t lux_high = (rx_buf[3] << 8) | rx_buf[4];  // High 16 bits
+    uint16_t lux_low  = (rx_buf[5] << 8) | rx_buf[6];  // Low 16 bits
+
+    // รวม High และ Low 16 bits เป็น 32-bit Illuminance
+    uint32_t lux = ((uint32_t)lux_high << 16) | lux_low;
+
+    // แสดงผล
+    printf("Illuminance: %lu lux\r\n", lux);
+}
+
+void TempHumi() {
+	Build_Modbus_Request(0x01, 0x0000, 0x0004, modbus_frame);
+	HAL_UART_Transmit(&huart1, modbus_frame, 8, 1000);
+	HAL_UART_Receive(&huart1, rx_buffer, 16, 1000);
+
+	printf("TEMPHUMI: ");
+	for (int i = 0; i < 16; i++) {
+	    printf("%d ", rx_buffer[i]);
+	}
+    printf("\r\n");
+
+    Decode_TempHumi(rx_buffer);
+
+	HAL_Delay(1000);
+}
+
+void Lihgt() {
+	Build_Modbus_Request(0x02, 0x0000, 0x0002, modbus_frame);
+	HAL_UART_Transmit(&huart1, modbus_frame, 8, 1000);
+	HAL_UART_Receive(&huart1, rx_buffer, 16, 1000);
+
+	printf("LiGHT: ");
+	for (int i = 0; i < 16; i++) {
+	    printf("%d ", rx_buffer[i]);
+	}
+    printf("\r\n");
+
+    Decode_Light(rx_buffer);
+
+	HAL_Delay(1000);
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ stm32_wheatherstation/Debug/Core/Startup/startup_stm32wle4jcix.d
@@ -0,0 +1,2 @@
+Core/Startup/startup_stm32wle4jcix.o: \
+ ../Core/Startup/startup_stm32wle4jcix.s
